2d5a62fc2818c07b558b41ee41773a5a3ab856ac
Ng Zhili <69728128+ngzhili@users.noreply.github.com>
2021-10-13 20:29:55 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,186 @@
+from flask import Flask,render_template,Response,request
+import cv2
+import time
+
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import mediapipe as mp
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+#from tensorflow.keras.callbacks import TensorBoard
+video_id = 'no'
+
+app=Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'POST':
+        if request.form.get('LSTM_model') == 'start_detection':
+            #print('hello123')
+            video_id = 'lstm'
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+# Use Holistic Models for detections
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+# Make keypoint detection, model can only detect in RGB
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB as model can only detect in RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Use Model to make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_landmarks(image, results): # draw landmarks for each image/frame
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS) # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+    
+    
+def draw_styled_landmarks(image, results): # draw landmarks for each image/frame, fix colour of landmark drawn
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             ) 
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=2), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=1)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=2), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=1)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=1)
+                             ) 
+    
+# define extract keypoint function
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh]) # concatenate all the keypoints that are flattened
+
+# Actions that we try to detect
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+
+label_map = {label:num for num, label in enumerate(actions)} #create label map dictionary
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662))) #each video has input shape of 30 frames of 1662 keypoints: X.shape
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu')) #next layer is a dense layer so we do not return sequences here
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+
+print('Loading Model...')
+model.load_weights('first_model_action.h5')
+print('Model Loaded!')
+
+colors = [(245,117,16), (117,245,16), (16,117,245)]
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1) #change length of bar depending on probability
+        cv2.putText(output_frame, actions[num]+' '+str(round(prob*100,2))+'%', (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        #cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
+    return output_frame
+
+
+   
+def gen():
+    # 1. New detection variables
+    sequence = []
+    sentence = []
+    predictions = []
+    threshold = 0.5
+    """Video streaming generator function."""
+    cap = cv2.VideoCapture(0)
+    sent =''
+    
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        # Read until video is completed
+        while(cap.isOpened()):
+        # Capture frame-by-frame
+            ret, image = cap.read()
+            if ret == True:
+                #img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+
+                # Make detections
+                image, results = mediapipe_detection(image, holistic)
+                #print(results)
+
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+
+
+                # 2. Prediction logic
+                keypoints = extract_keypoints(results)
+                sequence.append(keypoints)
+                sequence = sequence[-30:]
+                
+                if len(sequence) == 30:
+                    res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                    #print(actions[np.argmax(res)])
+                    predictions.append(np.argmax(res))
+                    
+                #3. Viz logic
+                    if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                        if res[np.argmax(res)] > threshold: 
+                            
+                            if len(sentence) > 0: 
+                                # if action is not in the last sentence, then we append the last action to the sentence
+                                if actions[np.argmax(res)] != sentence[-1]: 
+                                    sentence.append(actions[np.argmax(res)])
+                            else:
+                                sentence.append(actions[np.argmax(res)])
+
+                    if len(sentence) > 5: 
+                        sentence = sentence[-5:]
+                    
+                    # Viz probabilities
+                    image = prob_viz(res, actions, image, colors)
+                    
+                cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+                cv2.putText(image, ' '.join(sentence), (3,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+
+                #encode output image to bytes
+                frame = cv2.imencode('.jpg', image)[1].tobytes()
+
+                yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                #time.sleep(0.1)
+
+            else: 
+                break
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+
+    return Response(gen(),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
